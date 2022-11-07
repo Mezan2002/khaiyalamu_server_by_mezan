f6b1b6ea2e988c871dc574fa1543cfcdc1dad80a
Mezan2002 <mezanurr009@gmail.com>
2022-11-08 03:45:58 +0600

    Mongo DB added and connect done by user and password

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require start
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // middlewares end
+
+// mongo DB connect API start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// mongo DB connect API end
 
 app.use("/", (req, res) => {
   res.send("khaiyalamu server is running!!!");
